a0a0031cf48569384d8e9a5a4d071b78be41cd35
youssefalb <72605819+youssefalb@users.noreply.github.com>
2022-01-08 14:13:52 +0100

    QUIZ

---
+++ cmake-build-debug/CMakeFiles/pepa.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/Answer.cpp" "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/cmake-build-debug/CMakeFiles/pepa.dir/Answer.cpp.o"
+  "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/FileManager.cpp" "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/cmake-build-debug/CMakeFiles/pepa.dir/FileManager.cpp.o"
+  "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/Question.cpp" "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/cmake-build-debug/CMakeFiles/pepa.dir/Question.cpp.o"
+  "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/Quiz.cpp" "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/cmake-build-debug/CMakeFiles/pepa.dir/Quiz.cpp.o"
+  "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/main.cpp" "/mnt/c/Users/youssef albali/CLionProjects/QUIZ_GAME/cmake-build-debug/CMakeFiles/pepa.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pepa.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pepa.dir/Answer.cpp.o"
+  "CMakeFiles/pepa.dir/FileManager.cpp.o"
+  "CMakeFiles/pepa.dir/Question.cpp.o"
+  "CMakeFiles/pepa.dir/Quiz.cpp.o"
+  "CMakeFiles/pepa.dir/main.cpp.o"
+  "pepa"
+  "pepa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pepa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
